bcc1cd9f4f1a7a917f1ae7410b18b07d8603eb35
Daniel Coady <daniel.coady@outlook.com.au>
2019-05-26 11:44:33 +1000

    got lazy, made it work with mariadb for now

--- edgedevice/main.py
+++ edgedevice/main.py
@@ -1,18 +1,13 @@
 import serial
+import mysql.connector as mariadb
 import json
-import _thread
-from flask import Flask
-from flask_restful import Resource, Api
 from logger import *
+import settings
 
 set_logging(3)
-app = Flask(__name__)
-api = Api(app)
-inputs = None
 
 def main():
-    global inputs
-
+    # connect to arduino
     try:
         arduino = serial.Serial('/dev/ttyACM0', 9600)
         log('arduino connected')
@@ -20,29 +15,32 @@ def main():
         log('arduino not found, exiting...', 3)
         return
 
+    # connect to mariadb
+    try:
+        conn = mariadb.connect(user=settings.username, password=settings.password, database="badcontroller")
+        cursor = conn.cursor(buffered=True)
+        log('connected to mariadb')
+    except:
+        log('couldn\'t connect to mariadb, exiting...', 3)
+        return
+
     # main loop
     while True:
         try:
             inputs = json.loads(arduino.readline())
             log(inputs)
+            
+            query = 'UPDATE inputs SET usonic = ' + str(inputs['usonic'])
+            query += ', ldr = ' + str(inputs['ldr'])
+            query += ', temp = ' + str(inputs['temp'])
+            query += ', joyx = ' + str(inputs['joyx'])
+            query += ', joyy = ' + str(inputs['joyy'])
+            query += ', button = ' + str(inputs['button'])
+            cursor.execute(query)
+            conn.commit()
+
         except (json.decoder.JSONDecodeError, UnicodeDecodeError):
             log('couln\'t parse json for some reason idunno, will try again', 2)
 
-class InputAPI(Resource):
-    def get(self):
-        return {
-                'usonic' : inputs['usonic'],
-                'ldr' : inputs['ldr'],
-                'temp' : inputs['temp'],
-                'joyx' : inputs['joyx'],
-                'joyy' : inputs['joyy'],
-                'button' : inputs['button']
-               }
-
-def api_thread():
-    app.run(host='0.0.0.0', port=5000, debug=True)
-
 if __name__ == '__main__':
-    api.add_resource(InputAPI, '/')
-    api_thread()
-    _thread.start_new_thread(main, ())
+    main()
